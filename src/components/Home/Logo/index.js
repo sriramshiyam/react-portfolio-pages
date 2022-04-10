be8de573b20d288f8befedcce6459723a301d5cb
sriram <sriramshiyam04@gmail.com>
2022-04-10 10:44:14 +0530

    updated

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -25,7 +25,7 @@ const Logo = () => {
     gsap.fromTo(
       solidLogoRef.current,
       { opacity: 0 },
-      { opacity: 1, delay: 4, duration: 4  }
+      { opacity: 1, delay: 4, duration: 4 }
     )
   }, [])
   return (
